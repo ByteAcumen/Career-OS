a2569cadc3f3d916e990e432eaac3cd82d7340fc
ByteAcumen <hemantharish04@gmail.com>
2026-04-04 00:40:41 +0530

    Cleanup scratch files causing build failure

--- check-client-methods.ts
+++
@@ -1,9 +0,0 @@
-import { createAuthClient } from "better-auth/react";
-import { emailAndPasswordClient } from "better-auth/client/plugins";
-
-const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
-  plugins: [emailAndPasswordClient()],
-});
-
-console.log("Client Methods:", Object.keys(authClient.emailAndPassword || {}));
